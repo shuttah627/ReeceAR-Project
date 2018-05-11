3f2c5c204e13b6d6485c784b0a53ed9187ae8825
TaliesinNicholas <taliesyn.nicholas@hotmail.com>
2018-05-11 22:57:51 +1000

    Started Menu system

---
+++ Reece AR Project/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+    // Open default scene
+    public void OpenMainAR()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Quit
+    public void QuitApplication()
+    {
+        Application.Quit();
+    }
+}
