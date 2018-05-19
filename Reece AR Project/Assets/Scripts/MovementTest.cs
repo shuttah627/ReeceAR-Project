d9514463430245d7d744d88ca8cbed6ca76b254e
Michael Barnett <mbbarnet@deakin.edu.au>
2018-05-19 14:42:42 +1000

    Added my motiontest thing.

---
+++ Reece AR Project/Assets/Scripts/MovementTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTest : MonoBehaviour {
+
+    public Transform _toilet;
+
+	void Update () {
+        RotateShit();
+	}
+
+    public void RotateShit()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("Moved the toilet a bit");
+            _toilet.localPosition += new Vector3(.2f, 0f, 0f);
+
+            _toilet.Rotate(new Vector3(.04325f, 0.5f, 0.1f), 10f);
+        }
+    }
+}
