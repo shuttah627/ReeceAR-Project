b50ab58e3720691872390fbd27e58fd505d399e7
rollerbot <lochiejohnson@y7mail.com>
2018-05-20 19:36:23 +1000

    readded the movement scripts for the buttons -not able to get spawned objects dynamicly though.

--- Reece AR Project/Assets/Scripts/MovementTest.cs
+++ Reece AR Project/Assets/Scripts/MovementTest.cs
@@ -20,4 +20,26 @@ public class MovementTest : MonoBehaviour {
             _toilet.Rotate(new Vector3(.04325f, 0.5f, 0.1f), 10f);
         }
     }
+
+    public void MoveBack()
+    {
+        Debug.Log("moved the game object a bit");
+        _toilet.localPosition += new Vector3(0f, 0f, .1f);
+    }
+    public void MoveForward()
+    {
+        Debug.Log("moved the game object a bit");
+        _toilet.localPosition += new Vector3(0f, 0f, -.1f);
+    }
+    public void MoveLeft()
+    {
+        Debug.Log("moved the game object a bit");
+        _toilet.localPosition += new Vector3(-0.1f, 0f, 0f);
+    }
+    public void MoveRight()
+    {
+        Debug.Log("moved the game object a bit");
+        _toilet.localPosition += new Vector3(0.1f, 0f, 0f);
+    }
+
 }
