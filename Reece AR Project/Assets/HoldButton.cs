7177e5d0d208f08bdf375bf47d098d26700b052a
Michael Barnett <mbbarnet@deakin.edu.au>
2018-08-15 00:31:55 +1000

    Remade the MainAR UI. Hold the movement icon and move the camera. Hold the rotator in each direction.

---
+++ Reece AR Project/Assets/HoldButton.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoldButton : MonoBehaviour {
+
+    public GameObject _modelController;
+    bool _pressedDownMove = false;
+    bool _pressedDownLeft = false;
+    bool _pressedDownRight = false;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (_pressedDownMove)
+        {
+            _modelController.GetComponent<ModelController>().MoveFurniture("point");
+        }
+        if (_pressedDownLeft)
+        {
+            _modelController.GetComponent<ModelController>().RotateFurniture("left");
+        }
+        if (_pressedDownRight)
+        {
+            _modelController.GetComponent<ModelController>().RotateFurniture("right");
+        }
+    }
+
+    public void OnPressMove()
+    {
+        _pressedDownMove = true;
+    }
+
+    public void OnReleaseMove()
+    {
+        _pressedDownMove = false;
+    }
+
+    public void OnPressLeft()
+    {
+        _pressedDownLeft = true;
+    }
+
+    public void OnReleaseLeft()
+    {
+        _pressedDownLeft = false;
+    }
+
+    public void OnPressRight()
+    {
+        _pressedDownRight = true;
+    }
+
+    public void OnReleaseRight()
+    {
+        _pressedDownRight = false;
+    }
+}
