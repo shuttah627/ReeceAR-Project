37101187ca2c4780e24da96220c704e0f44b8ba7
TaliesinNicholas <taliesyn.nicholas@hotmail.com>
2018-09-23 21:13:32 +1000

    UI changes to scripting

--- Reece AR Project/Assets/Off-Canvas Menu/Scripts/OCMController.cs
+++ Reece AR Project/Assets/Off-Canvas Menu/Scripts/OCMController.cs
@@ -1,4 +1,91 @@
-﻿using System.Collections;
+﻿using System.Collections; 
+using System.Collections.Generic; // Added
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(ScrollRect))]
+public class OCMController : MonoBehaviour
+{
+    public float OpenDragThreshold = 0.35f;
+    public float TransitionSpeed = 0.5f;
+    public Color OverlayColor = new Color(0, 0, 0, 0.8f);
+    public Image DragHandle;
+    public Image TransparentPadding;
+
+    // Added
+    public List<GameObject> panels = new List<GameObject>();
+    int panelsCount = 4;
+
+    private ScrollRect scrollRect;
+    private Color transparentColor;
+
+	void Start()
+	{
+        transparentColor = new Color(OverlayColor.r, OverlayColor.g, OverlayColor.b, 0);
+        scrollRect = GetComponent<ScrollRect>();
+        Close(); // menu minimised on startup
+	}
+
+    void Update()
+    {
+        DragHandle.color = Color.Lerp(transparentColor, OverlayColor, 1 - scrollRect.horizontalNormalizedPosition);
+        TransparentPadding.color = Color.Lerp(transparentColor, OverlayColor, 1 - scrollRect.horizontalNormalizedPosition);
+    }
+    
+    public void Open()
+    {
+        // if not already, activate default panel...
+        if (!panels[0].activeInHierarchy)
+            panels[0].SetActive(true); 
+
+        // open side menu
+        StartCoroutine(SlideTo(0, TransitionSpeed));        
+    }
+
+    public void Close()
+    {
+        StartCoroutine(SlideTo(1, TransitionSpeed));
+        CloseAll(); // close all panels
+        panels[0].SetActive(true); // activate catelogories...
+    }
+
+    // Close all
+    public void CloseAll()
+    {
+        for (int i = 0; i < panelsCount; i++)
+            panels[i].SetActive(false);
+    }
+
+    private IEnumerator SlideTo(float endXPos, float time)
+    {
+        var startXPos = scrollRect.horizontalNormalizedPosition;
+
+        var i = 0.0f;
+        var rate = 1.0f / time;
+        while (i < 1.0f)
+        {
+            i += Time.deltaTime * rate;
+            scrollRect.horizontalNormalizedPosition = Mathf.Lerp(startXPos, endXPos, i);
+            yield return null;
+        }
+    }
+
+    public void OnEndDrag()
+    {
+        if (scrollRect.horizontalNormalizedPosition < OpenDragThreshold)
+        {
+            Open();
+        }
+        else
+        {
+            Close();
+        }
+    }
+}
+
+// Defualt
+/*
+ * using System.Collections;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -62,3 +149,4 @@ public class OCMController : MonoBehaviour
         }
     }
 }
+*/
--- Reece AR Project/Assets/Off-Canvas Menu/Scripts/OCMItem.cs
+++ Reece AR Project/Assets/Off-Canvas Menu/Scripts/OCMItem.cs
@@ -13,3 +13,23 @@ public class OCMItem : MonoBehaviour
         Debug.Log(text);
     }
 }
+
+// Default
+/*
+ * using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OCMItem : MonoBehaviour
+{
+    public void ChangeScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+
+    public void DebugLog(string text)
+    {
+        Debug.Log(text);
+    }
+}
+
+    */
